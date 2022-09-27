cd41460856d8861f07ab72711f40ee9d97e32c8b
Xires87 <frycmod@gmail.com>
2022-09-27 11:57:52 +0200

    increased compatibility with other mods

--- src/main/java/net/fryc/hammersandtables/mixin/VillagerEntityMixin.java
+++ src/main/java/net/fryc/hammersandtables/mixin/VillagerEntityMixin.java
@@ -6,11 +6,14 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.InteractionObserver;
 import net.minecraft.entity.passive.MerchantEntity;
 import net.minecraft.entity.passive.VillagerEntity;
-import net.minecraft.village.TradeOfferList;
+import net.minecraft.village.TradeOffers;
 import net.minecraft.village.VillagerData;
 import net.minecraft.village.VillagerDataContainer;
+import net.minecraft.village.VillagerProfession;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(VillagerEntity.class)
 abstract class VillagerEntityMixin extends MerchantEntity implements InteractionObserver, VillagerDataContainer {
@@ -20,18 +23,19 @@ abstract class VillagerEntityMixin extends MerchantEntity implements Interaction
         super(entityType, world);
     }
 
-    //Overwrites vanilla trade offers
-    @Override
-    public void fillRecipes() {
-        VillagerData villagerData = this.getVillagerData();
-        Int2ObjectMap<ModTradeOffers.Factory[]> int2ObjectMap = (Int2ObjectMap) ModTradeOffers.PROFESSION_TO_LEVELED_TRADE.get(villagerData.getProfession());
-        if (int2ObjectMap != null && !int2ObjectMap.isEmpty()) {
-            ModTradeOffers.Factory[] factorys = (ModTradeOffers.Factory[])int2ObjectMap.get(villagerData.getLevel());
-            if (factorys != null) {
-                TradeOfferList tradeOfferList = this.getOffers();
-                this.fillRecipesFromPool(tradeOfferList, factorys, 2);
+    //Overwrites vanilla trade offers for Armorer, Toolsmith, Librarian and Weaponsmith
+    @ModifyVariable(method = "fillRecipes()V", at = @At("STORE"))
+    private Int2ObjectMap<TradeOffers.Factory[]> injected(Int2ObjectMap<TradeOffers.Factory[]> int2ObjectMap) {
+        if(int2ObjectMap != null){
+            VillagerData villagerData = ((VillagerEntity)(Object)this).getVillagerData();
+            if(villagerData.getProfession() == VillagerProfession.ARMORER || villagerData.getProfession() == VillagerProfession.TOOLSMITH ||
+                    villagerData.getProfession() == VillagerProfession.LIBRARIAN || villagerData.getProfession() == VillagerProfession.WEAPONSMITH){
+                int2ObjectMap.clear();
+                int2ObjectMap = ModTradeOffers.PROFESSION_TO_LEVELED_TRADE.get(villagerData.getProfession());
             }
         }
+        return int2ObjectMap;
     }
+
 
 }
