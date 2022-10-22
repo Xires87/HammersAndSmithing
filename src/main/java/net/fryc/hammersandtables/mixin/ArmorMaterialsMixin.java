4691ac52b78c05da2283fb4eab8c30bf0e8c2170
Xires87 <frycmod@gmail.com>
2022-10-22 13:44:58 +0200

    improved code

--- src/main/java/net/fryc/hammersandtables/mixin/ArmorMaterialsMixin.java
+++ src/main/java/net/fryc/hammersandtables/mixin/ArmorMaterialsMixin.java
@@ -17,21 +17,21 @@ abstract class ArmorMaterialsMixin implements ArmorMaterial {
     @Inject(method = "getProtectionAmount(Lnet/minecraft/entity/EquipmentSlot;)I", at = @At("HEAD"), cancellable = true)
     private void armor(EquipmentSlot slot,CallbackInfoReturnable<Integer> dur) {
         ArmorMaterials dys = ((ArmorMaterials)(Object)this);
-        if(slot.getEntitySlotId() == 0 && dys.getEnchantability() == 12) dur.setReturnValue(2);
+        if(slot.getEntitySlotId() == 0 && dys.equals(ArmorMaterials.CHAIN)) dur.setReturnValue(2);
     }
 
     //sets chainmal armors toughness to 0.75
     @Inject(method = "getToughness()F", at = @At("HEAD"), cancellable = true)
     private void toughness(CallbackInfoReturnable<Float> dur) {
         ArmorMaterials dys = ((ArmorMaterials)(Object)this);
-        if(dys.getEnchantability() == 12) dur.setReturnValue(0.75F);
+        if(dys.equals(ArmorMaterials.CHAIN)) dur.setReturnValue(0.75F);
     }
 
     //sets repair ingredient of chainmail armor to chain
     @Inject(method = "getRepairIngredient()Lnet/minecraft/recipe/Ingredient;", at = @At("HEAD"), cancellable = true)
     private void repair(CallbackInfoReturnable<Ingredient> dur) {
         ArmorMaterials dys = ((ArmorMaterials)(Object)this);
-        if(dys.getEnchantability() == 12) dur.setReturnValue(Ingredient.ofItems(Items.CHAIN));
+        if(dys.equals(ArmorMaterials.CHAIN)) dur.setReturnValue(Ingredient.ofItems(Items.CHAIN));
     }
 
 }
--- src/main/java/net/fryc/hammersandtables/mixin/ToolMaterialsMixin.java
+++ src/main/java/net/fryc/hammersandtables/mixin/ToolMaterialsMixin.java
@@ -16,27 +16,27 @@ abstract class ToolMaterialsMixin implements ToolMaterial {
     @Inject(method = "getDurability()I", at = @At("HEAD"), cancellable = true)
     private void durability(CallbackInfoReturnable<Integer> dur) {
         ToolMaterials dys = ((ToolMaterials)(Object)this);
-        if(dys.getEnchantability() == 22) dur.setReturnValue(26);
-        else if(dys.getMiningSpeedMultiplier() == 2.0F) dur.setReturnValue(6);
-        else if(dys.getMiningLevel() == 1) dur.setReturnValue(17);
+        if(dys.equals(ToolMaterials.GOLD)) dur.setReturnValue(26);
+        else if(dys.equals(ToolMaterials.WOOD)) dur.setReturnValue(6);
+        else if(dys.equals(ToolMaterials.STONE)) dur.setReturnValue(17);
     }
 
     //changes mining levels of vanilla iron, diamond and netherite tools
     @Inject(method = "getMiningLevel()I", at = @At("HEAD"), cancellable = true)
     private void level(CallbackInfoReturnable<Integer> dur) {
         ToolMaterials dys = ((ToolMaterials)(Object)this);
-        if(dys.getEnchantability() == 14) dur.setReturnValue(3);
-        else if(dys.getEnchantability() == 10) dur.setReturnValue(5);
-        else if(dys.getMiningSpeedMultiplier() == 9.0F) dur.setReturnValue(6);
+        if(dys.equals(ToolMaterials.IRON)) dur.setReturnValue(3);
+        else if(dys.equals(ToolMaterials.DIAMOND)) dur.setReturnValue(5);
+        else if(dys.equals(ToolMaterials.NETHERITE)) dur.setReturnValue(6);
     }
 
     //changes attack damage of wooden, stone and iron tools
     @Inject(method = "getAttackDamage()F", at = @At("HEAD"), cancellable = true)
     private void damage(CallbackInfoReturnable<Float> dur) {
         ToolMaterials dys = ((ToolMaterials)(Object)this);
-        if(dys.getMiningSpeedMultiplier() == 2.0F) dur.setReturnValue(-2.0F);
-        else if(dys.getMiningSpeedMultiplier() == 4.0F) dur.setReturnValue(-1.0F);
-        else if(dys.getMiningSpeedMultiplier() == 6.0F) dur.setReturnValue(1.0F);
+        if(dys.equals(ToolMaterials.WOOD)) dur.setReturnValue(-2.0F);
+        else if(dys.equals(ToolMaterials.STONE)) dur.setReturnValue(-1.0F);
+        else if(dys.equals(ToolMaterials.IRON)) dur.setReturnValue(1.0F);
     }
 
 }
