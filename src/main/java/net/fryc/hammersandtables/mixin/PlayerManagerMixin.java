440db97f535c02721b2fc9414ee77009e95c006f
Xires87 <frycmod@gmail.com>
2023-12-07 11:50:32 +0100

    version + fixes

--- src/main/java/net/fryc/hammersandtables/mixin/PlayerManagerMixin.java
+++ src/main/java/net/fryc/hammersandtables/mixin/PlayerManagerMixin.java
@@ -8,7 +8,6 @@ import net.fryc.hammersandtables.recipes.ModSmithingRecipes;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.PlayerManager;
-import net.minecraft.server.network.ConnectedClientData;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,11 +17,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(PlayerManager.class)
 abstract class PlayerManagerMixin {
 
-    @Inject(method = "onPlayerConnect(Lnet/minecraft/network/ClientConnection;Lnet/minecraft/server/network/ServerPlayerEntity;Lnet/minecraft/server/network/ConnectedClientData;)V", at = @At("HEAD"))
-    private void informPlayerAboutSmithingRecipes(ClientConnection connection, ServerPlayerEntity player, ConnectedClientData clientData, CallbackInfo info) {
+    @Inject(method = "onPlayerConnect(Lnet/minecraft/network/ClientConnection;Lnet/minecraft/server/network/ServerPlayerEntity;)V", at = @At("HEAD"))
+    private void informPlayerAboutSmithingRecipes(ClientConnection connection, ServerPlayerEntity player, CallbackInfo info) {
         //sending server's map to client
         PacketByteBuf buf = PacketByteBufs.create();
-        buf.writeMap(ModSmithingRecipes.smithingAdditionalVariables, PacketByteBuf::writeInt, PacketByteBuf::writeIntArray);
+        buf.writeMap(ModSmithingRecipes.smithingAdditionalVariables, PacketByteBuf::writeString, PacketByteBuf::writeIntArray);
         connection.send(ServerPlayNetworking.createS2CPacket(ModPackets.HAMMERS_AND_SMITHING_NETWORKING, buf));
 
         //sending some server's config options to client
