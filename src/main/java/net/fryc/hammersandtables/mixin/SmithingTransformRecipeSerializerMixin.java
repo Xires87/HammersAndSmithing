0ecfc035ee6853572a80cf1159e4ba4ac6e7af9f
Xires87 <frycmod@gmail.com>
2024-09-13 12:54:51 +0200

    changed directories names

--- src/main/java/net/fryc/hammersandtables/mixin/SmithingTransformRecipeSerializerMixin.java
+++ src/main/java/net/fryc/hammersandtables/mixin/SmithingTransformRecipeSerializerMixin.java
@@ -1,15 +1,14 @@
 package net.fryc.hammersandtables.mixin;
 
 
-import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.fryc.hammersandtables.recipes.ModSmithingRecipes;
 import net.fryc.hammersandtables.util.SmithingTransformAdditionalVariables;
 import net.fryc.hammersandtables.util.TransformRecipeNumber;
 import net.minecraft.item.ItemStack;
-import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.RegistryByteBuf;
 import net.minecraft.recipe.Ingredient;
-import net.minecraft.recipe.RecipeCodecs;
 import net.minecraft.recipe.RecipeSerializer;
 import net.minecraft.recipe.SmithingTransformRecipe;
 import net.minecraft.util.dynamic.Codecs;
@@ -18,11 +17,32 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.List;
+
 
 @Mixin(SmithingTransformRecipe.Serializer.class)
 abstract class SmithingTransformRecipeSerializerMixin implements RecipeSerializer<SmithingTransformRecipe> {
 
 
+    private static final MapCodec<SmithingTransformRecipe> CODEC = RecordCodecBuilder.mapCodec((instance) -> {
+        return instance.group(Ingredient.ALLOW_EMPTY_CODEC.fieldOf("template").forGetter((recipe) -> {
+            return ((SmithingTransformAdditionalVariables) recipe).getSmithingTemplate();
+        }), Ingredient.ALLOW_EMPTY_CODEC.fieldOf("base").forGetter((recipe) -> {
+            return ((SmithingTransformAdditionalVariables) recipe).getSmithingBase();
+        }), Ingredient.ALLOW_EMPTY_CODEC.fieldOf("addition").forGetter((recipe) -> {
+            return ((SmithingTransformAdditionalVariables) recipe).getSmithingAddition();
+        }), ItemStack.VALIDATED_CODEC.fieldOf("result").forGetter((recipe) -> {
+            return ((SmithingTransformAdditionalVariables) recipe).getSmithingResult();
+        }), Codecs.POSITIVE_INT.fieldOf("additionCount").orElse(1).forGetter((additionCount) -> {
+            return ((SmithingTransformAdditionalVariables) additionCount).getAdditionCount();
+        }), Codecs.POSITIVE_INT.fieldOf("tableTier").orElse(0).forGetter((tableTier) -> {
+            return ((SmithingTransformAdditionalVariables) tableTier).getTableTier();
+        }), Codecs.POSITIVE_INT.fieldOf("hammerTier").orElse(0).forGetter((hammerTier) -> {
+            return ((SmithingTransformAdditionalVariables) hammerTier).getHammerTier();
+        })).apply(instance, ModSmithingRecipes::createNewSmithingTransformRecipe);
+    });
+
+    /*
     private static final Codec<SmithingTransformRecipe> CODEC = RecordCodecBuilder.create((instance) -> {
         return instance.group(Ingredient.ALLOW_EMPTY_CODEC.fieldOf("template").forGetter((recipe) -> {
             return ((SmithingTransformAdditionalVariables) recipe).getSmithingTemplate();
@@ -40,40 +60,27 @@ abstract class SmithingTransformRecipeSerializerMixin implements RecipeSerialize
             return ((SmithingTransformAdditionalVariables) hammerTier).getHammerTier();
         })).apply(instance, ModSmithingRecipes::createNewSmithingTransformRecipe);
     });
+     */
 
-    @Inject(method = "codec()Lcom/mojang/serialization/Codec;", at = @At("HEAD"), cancellable = true)
-    private void replacingCodec(CallbackInfoReturnable<Codec<SmithingTransformRecipe>> ret) {
+    @Inject(method = "codec()Lcom/mojang/serialization/MapCodec;", at = @At("HEAD"), cancellable = true)
+    private void replacingCodec(CallbackInfoReturnable<MapCodec<SmithingTransformRecipe>> ret) {
         ret.setReturnValue(CODEC);
     }
 
 
-    /* old
-    @Inject(method = "read(Lnet/minecraft/util/Identifier;Lcom/google/gson/JsonObject;)Lnet/minecraft/recipe/SmithingTransformRecipe;", at = @At("HEAD"), cancellable = true)
-    private void readingSmithingRecipesFromJson(Identifier identifier, JsonObject jsonObject, CallbackInfoReturnable<SmithingTransformRecipe> ret) {
-        Ingredient ingredient = Ingredient.fromJson(JsonHelper.getElement(jsonObject, "template"));
-        Ingredient ingredient2 = Ingredient.fromJson(JsonHelper.getElement(jsonObject, "base"));
-        Ingredient ingredient3 = Ingredient.fromJson(JsonHelper.getElement(jsonObject, "addition"));
-        ItemStack itemStack = ShapedRecipe.outputFromJson(JsonHelper.getObject(jsonObject, "result"));
-        int additionCount = JsonHelper.getInt(jsonObject, "additionCount", 1);
-        int tableTier = JsonHelper.getInt(jsonObject, "tableTier", 0);
-        int hammerTier = JsonHelper.getInt(jsonObject, "hammerTier", tableTier);
-        ret.setReturnValue(ModSmithingRecipes.createNewSmithingTransformRecipe(ingredient, ingredient2, ingredient3, itemStack, additionCount, tableTier, hammerTier));
-    }
-    */
-
-
     //reading additional variables for smithing recipes from map
-    @Inject(method = "read(Lnet/minecraft/network/PacketByteBuf;)Lnet/minecraft/recipe/SmithingTransformRecipe;", at = @At("HEAD"), cancellable = true)
-    private void readingSmithingRecipesFromPacketByteBuf(PacketByteBuf packetByteBuf, CallbackInfoReturnable<SmithingTransformRecipe> ret) {
-        Ingredient ingredient = Ingredient.fromPacket(packetByteBuf);
-        Ingredient ingredient2 = Ingredient.fromPacket(packetByteBuf);
-        Ingredient ingredient3 = Ingredient.fromPacket(packetByteBuf);
-        ItemStack itemStack = packetByteBuf.readItemStack();
-        int[] vars = ModSmithingRecipes.smithingAdditionalVariables.get(TransformRecipeNumber.number);
+    @Inject(method = "read(Lnet/minecraft/network/RegistryByteBuf;)Lnet/minecraft/recipe/SmithingTransformRecipe;", at = @At("HEAD"), cancellable = true)
+    private void readingSmithingRecipesFromPacketByteBuf(RegistryByteBuf buf, CallbackInfoReturnable<SmithingTransformRecipe> ret) {
+        Ingredient ingredient = (Ingredient)Ingredient.PACKET_CODEC.decode(buf);
+        Ingredient ingredient2 = (Ingredient)Ingredient.PACKET_CODEC.decode(buf);
+        Ingredient ingredient3 = (Ingredient)Ingredient.PACKET_CODEC.decode(buf);
+        ItemStack itemStack = (ItemStack)ItemStack.PACKET_CODEC.decode(buf);
+
+        List<Integer> vars = ModSmithingRecipes.smithingAdditionalVariables.get(TransformRecipeNumber.number);
         TransformRecipeNumber.number++;
-        int additionCount = vars[0];
-        int tableTier = vars[1];
-        int hammerTier = vars[2];
+        int additionCount = vars.get(0);
+        int tableTier = vars.get(1);
+        int hammerTier = vars.get(2);
         ret.setReturnValue(ModSmithingRecipes.createNewSmithingTransformRecipe(ingredient, ingredient2, ingredient3, itemStack, additionCount, tableTier, hammerTier));
     }
 
