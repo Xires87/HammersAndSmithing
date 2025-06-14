9323223e7f8e4f912318a0ae36ed52f6fbd75aa1
Xires87 <frycmod@gmail.com>
2025-06-14 14:02:49 +0200

    tooltip for master tools

--- src/main/java/net/fryc/hammersandtables/items/custom/MasterToolsItem.java
+++ src/main/java/net/fryc/hammersandtables/items/custom/MasterToolsItem.java
@@ -1,11 +1,22 @@
 package net.fryc.hammersandtables.items.custom;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.text.Text;
+
+import java.util.List;
 
 public class MasterToolsItem extends Item implements HasHammerTier {
 
     public MasterToolsItem(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        super.appendTooltip(stack, context, tooltip, type);
+        tooltip.add(Text.translatable("text.hammersandtables.item.master_tools"));
     }
 
     @Override
